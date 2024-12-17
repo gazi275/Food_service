703218b5fdceb96326ffe9ede45fddca15088110
gazi275 <gaziazad270@gmail.com>
2024-12-18 01:01:45 +0600

    ready to production

--- src/store/useRestaurantStore.ts
+++ src/store/useRestaurantStore.ts
@@ -3,7 +3,7 @@
 import axiosInstance from "@/Axios/Axios";
 import { Orders } from "@/types/orderType";
 import { MenuItem, RestaurantState } from "@/types/restaurantType";
-import axios from "axios";
+
 import { toast } from "sonner";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
